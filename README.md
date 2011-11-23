737cd91d58916ccb18bd6f98b8edda25565e9ba7
Will Boyce <me@willboyce.com>
2011-11-23 19:56:39 +0000

    Added handy script for generating CA/certificates and updated README.

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-Sirious, the _totally sirious_ Siri Proxy.
+## Sirious, the _totally sirious_ Siri Proxy.
 
-Currently only really at the PoC stage, but it does unpack all the data and re-pack it to send it back so the groundwork is there for a tampering proxy.
 
-More documentation to come!
+### Usage
+
+First you'll need to install the biplist Python module:
+
+    $ pip install biplist
+
+Due to the way Siri works, you'll need to install a Custom CA onto your iPhone. There is a included script that will do most of the legwork for you, just run:
+
+    $ ./gen_certs.zsh
+
+It'll prompt you for some input, the correct answers are "1234", "1234", "y" and "y".
+Once this is done there will be keys/ca.pem - you need to get this file install on your iPhone, email is probably the easiest.
+During installation iOS will give you a pretty scary (and rightfully so) warning, you can safely ignore it as this is **your** CA, and hopefully you can trust yourself!
+
+Once this is done, you'll need to redirect the DNS for guzzoni.apple.com to wherever you are running sirious. I'd recommend using dnsmasq for this.
+Finally, fire up sirious:
+
+    $ sudo python main.py
---
+++ gen_certs.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+
+# Feel free to change any of these defaults
+countryName="UK"
+stateOrProvinceName="England"
+localityName=""
+organizationName="Will Boyce"
+organizationalUnitName=""
+commonName="Sirious"
+emailAddress=""
+
+## Do not edit below here!
+
+mkdir keys
+mkdir -p demoCA/{certs,crl,newcerts,private}
+touch demoCA/index.txt
+echo 01 > demoCA/crtnumber
+
+CAREQARGS="${countryName}\n${stateOrProvinceName}\n${localityName}\n${organizationName}\n${organizationalUnitName}\n${commonName}\n${emailAddress}\n\n\n"
+echo -n $CAREQARGS | openssl req -new -keyout demoCA/private/cakey.pem -out demoCA/careq.pem -passin pass:1234 -passout pass:1234
+openssl ca -create_serial -out demoCA/cacert.pem -days 1095 -batch -keyfile demoCA/private/cakey.pem -selfsign -extensions v3_ca -infiles demoCA/careq.pem
+
+CRTREQARGS="${countryName}\n${stateOrProvinceName}\n${localityName}\n${organizationName}\n${organizationalUnitName}\nguzzoni.apple.com\n${emailAddress}\n\n\n"
+echo $CRTREQARGS | openssl req -new -keyout newkey.pem -out newreq.pem -days 1095 -passin pass:1234 -passout pass:1234
+openssl ca -policy policy_anything -out newcert.pem -infiles newreq.pem
+openssl rsa -in newkey.pem -out server.key -passin pass:1234
+
+mv newcert.pem server.crt
+mv demoCA/cacert.pem keys/ca.pem
+mv server.{crt,key} keys/
+rm -rf new{key,req}.pem demoCA
