c5a9674fdd4bfa94783113be17ed7984902a59af
Will Boyce <me@willboyce.com>
2011-11-23 17:41:09 +0000

    Added example plugin & cfg

---
+++ sirious/plugins/sirious_test.py
@@ -0,0 +1,20 @@
+from sirious import SiriObjects
+
+
+class SiriousTest(object):
+    def __init__(self, proxy):
+        self.proxy = proxy
+
+    def process_plist(self, plist):
+        return plist
+
+    def known_intent(self, phrase, plist):
+        if phrase.lower().strip() == "are you serious":
+            self.proxy.blocking = True
+            root = SiriObjects.AddViews()
+            root.make_root(ref_id=self.proxy.ref_id)
+            root.views.append(SiriObjects.Utterance(text="You're damn right I am. Sirious is up and running!"))
+            self.proxy.inject_plist(root.to_dict())
+
+    def unknown_intent(self, phrase, plist):
+        pass
