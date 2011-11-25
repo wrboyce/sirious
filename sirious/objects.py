03421cbdc57420f21fb264c2015569900c5a0fd0
Will Boyce <me@willboyce.com>
2011-11-25 20:20:06 +0000

    Tidied up & documented SiriObjects

--- sirious/objects.py
+++ sirious/objects.py
@@ -66,6 +66,19 @@ class SiriObject(object):
 
 class SiriObjects(object):
     class AddViews(SiriObject):
+        """
+            A command received by the iPhone informing it to draw something
+
+            Properties:
+                `scrollToTop`
+                    Scroll to new views the top of the screen (I presume?)
+                `temporary`
+                    Doesn't act like I'd expect, not so sure of this one
+                `dialogPhase`
+                    Doesn't seem to matter too much, but varies
+                `views`
+                    A list of either `Utterance`, or `Wolfram` instances
+        """
         cls = "AddViews"
         scrollToTop = False
         temporary = False
@@ -73,9 +86,26 @@ class SiriObjects(object):
         views = []
 
     class Utterance(SiriObject):
+        """
+            The "Meat & Potatoes" of the SiriObjects, an Utterance prompts a
+            textual and (optionally) a voice prompt.
+
+            Properties:
+                `text`
+                    Text to display on screen.
+                `speakableText`
+                    Text to read, this will default to `text` if not provided.
+                    This field also supports various @tts{} commands.
+                `dialogIdentifier`
+                    I'm not convinced these do a whole lot, they seem to vary
+                    from things like Misc#ident (default) to
+                    Answer#someSpecificQuestion and not much else changes.
+                `listenAfterSpeaking`
+                    Somewhat self-explainatory, defaults to False.
+        """
         cls = "AssistantUtteranceView"
         text = ""
-        speakableText = ""
+        speakableText = None
         dialogIdentifier = "Misc#ident"
         listenAfterSpeaking = False
 
@@ -84,22 +114,50 @@ class SiriObjects(object):
             if self.speakableText is None:
                 self.speakableText = self.text
 
-    class Map(SiriObject):
-        cls = "MapItemSnippet"
-        group = "com.apple.ace.localsearch"
-        user_current_location = True
-        items = []
-
     class Wolfram(SiriObject):
+        """
+            The Wolfram Snippet, not surprisingly, shows Wolfram Results.
+            This SiriObject is also the best way to insert images.
+
+            Properties:
+                `answers`
+                    A list of `SiriObjects.Answer` instances
+        """
         cls = "Snippet"
         group = "com.apple.ace.answer"
         answers = []
 
-    class Button(SiriObject):
-        cls = "Button"
+    class Answer(SiriObject):
+        """
+            The Answer Object provides the "sections" of a Wolfram Answer
+
+            Properties:
+                `title`
+                    The title displayed on the purple background
+                `lines`
+                    A list of `SiriObject.AnswerLine` instances
+        """
+        cls = "Object"
+        group = "com.apple.ace.answer"
+        title = ""
+        lines = []
+
+    class AnswerLine(SiriObject):
+        """
+            The AnswerLine Object provides the content of a SiriObjects.Answer
+
+            Properties:
+                `text`
+                    Text to display
+                `image`
+                    URL of a image to display
+        """
+        cls = "ObjectLine"
+        group = "com.apple.ace.answer"
         text = ""
-        commands = []
+        image = ""
 
     class RequestCompleted(SiriObject):
+        """ Send at the end of a request, self-explainatory. """
         cls = "RequestCompleted"
         group = "com.apple.ace.system"
