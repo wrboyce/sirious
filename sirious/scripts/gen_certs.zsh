c2b5d0f24a7d09f0a01462ff6cf81041977e91c0
Will Boyce <me@willboyce.com>
2013-03-15 16:55:46 +0000

    fix gen_certs script (kinda)

--- sirious/scripts/gen_certs.zsh
+++ sirious/scripts/gen_certs.zsh
@@ -19,14 +19,12 @@ echo 01 > demoCA/crtnumber
 
 CAREQARGS="${countryName}\n${stateOrProvinceName}\n${localityName}\n${organizationName}\n${organizationalUnitName}\n${commonName}\n${emailAddress}\n\n\n"
 echo -n $CAREQARGS | openssl req -new -keyout demoCA/private/cakey.pem -out demoCA/careq.pem -passin pass:1234 -passout pass:1234
-openssl ca -create_serial -out demoCA/cacert.pem -days 1095 -batch -keyfile demoCA/private/cakey.pem -selfsign -extensions v3_ca -infiles demoCA/careq.pem
+openssl ca -create_serial -passin pass:1234 -out ssl/ca.pem -outdir demoCA/newcerts -days 1095 -batch -keyfile demoCA/private/cakey.pem -selfsign -extensions v3_ca -infiles demoCA/careq.pem
 
 CRTREQARGS="${countryName}\n${stateOrProvinceName}\n${localityName}\n${organizationName}\n${organizationalUnitName}\nguzzoni.apple.com\n${emailAddress}\n\n\n"
-echo $CRTREQARGS | openssl req -new -keyout newkey.pem -out newreq.pem -days 1095 -passin pass:1234 -passout pass:1234
-openssl ca -policy policy_anything -out newcert.pem -infiles newreq.pem
-openssl rsa -in newkey.pem -out server.key -passin pass:1234
+echo $CRTREQARGS | openssl req -new -keyout demoCA/newkey.pem -out demoCA/newreq.pem -days 1095 -passin pass:1234 -passout pass:1234
 
-mv newcert.pem server.crt
-mv demoCA/cacert.pem ssl/ca.pem
-mv server.{crt,key} ssl/
-rm -rf new{key,req}.pem demoCA
+openssl ca -policy policy_anything -out ssl/server.crt -passin pass:1234 -keyfile demoCA/private/cakey.pem -cert ssl/ca.pem -infiles demoCA/newreq.pem
+openssl rsa -in demoCA/newkey.pem -out ssl/server.key -passin pass:1234
+
+rm -rf demoCA/
